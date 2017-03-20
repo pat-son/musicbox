79b2c957013f077097ab2017331ab8f9f178284a
pat-son <patrick.son@comcast.net>
2017-03-20 17:15:16 -0400

    vertical bar in title is optional

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Include vertical bar in title only if there is a specific page title
+  def full_title(page_title = '')
+    base_title = "Music Box"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
