e8499641bead9b27c3a004295b3eab9325f8d3da
pat-son <patrick.son@comcast.net>
2017-03-21 12:48:03 -0400

    Users can create and view songs

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -1,10 +1,29 @@
 class CreationsController < ApplicationController
   def index
+    @creations = Creation.all
   end
   
   def new
+    @creation = Creation.new
   end
 
   def create
+    @creation = Creation.new(creation_params)
+    @creation.user_id = (current_user).id
+    if @creation.save
+      redirect_to creations_path
+    else
+      flash[:danger] = "Invalid information."
+      render 'new'
+    end
   end
+
+  def show
+    @creation = Creation.find(params[:id])
+  end
+
+  private
+    def creation_params
+      params.require(:creation).permit(:name, :data)
+    end
 end
